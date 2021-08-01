2cb38d0d4acfc28a9db1d8d925af6237ad9ef13a
tomsan96 <gradada0507@gmail.com>
2021-08-01 23:51:03 +0900

    空のviewModelファイル追加

---
+++ UmbrellaApp/Presentation/FortuneTelling/ForetuneTellingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ForetuneTellingViewModel.swift
+//  UmbrellaApp
+//
+//  Created by 山崎定知 on 2021/08/01.
+//
+
+import Foundation
---
+++ UmbrellaApp/Presentation/Menu/MenuViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MenuViewModel.swift
+//  UmbrellaApp
+//
+//  Created by 山崎定知 on 2021/08/01.
+//
+
+import Foundation
