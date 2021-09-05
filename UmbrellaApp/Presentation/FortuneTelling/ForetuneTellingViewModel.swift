79b94458ffb1b6652fbbc35ab4313c986e31703e
mht-sadatomo-yamasaki <s.yamasaki@mht-code.com>
2021-09-05 15:44:44 +0900

    占い用のview model作成

--- UmbrellaApp/Presentation/FortuneTelling/ForetuneTellingViewModel.swift
+++ UmbrellaApp/Presentation/FortuneTelling/ForetuneTellingViewModel.swift
@@ -5,4 +5,42 @@
 //  Created by 山崎定知 on 2021/08/01.
 //
 
-import Foundation
+import RxSwift
+import Moya
+
+class ForetuneTellingViewModel {
+    private let disposeBag: DisposeBag = DisposeBag()
+    let getWeatherInformationSubject = PublishSubject<(WeatherModel?, Error?)>()
+    
+    var selectedPrefecture: Observable<String> {
+        return selectedPrefectureSubject
+    }
+
+    private let selectedPrefectureSubject = PublishSubject<String>()
+
+    func set(text: String) {
+        selectedPrefectureSubject.onNext(text)
+    }
+    
+    func getWeatherInformation() {
+        _ = ApiRepository.shared.getWeatjerInformation().asObservable().subscribe(onNext: { (weatherModel) in
+            self.getWeatherInformationSubject.onNext((weatherModel, nil))
+            }, onError: { (error) in
+                do {
+                    let errorResponse = error as? Moya.MoyaError
+                    if let body = try errorResponse?.response?.mapJSON() {
+                        print(body)
+                        print("---")
+                        print(error)
+
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        )
+        .disposed(by: disposeBag)
+    }
+
+}
+
